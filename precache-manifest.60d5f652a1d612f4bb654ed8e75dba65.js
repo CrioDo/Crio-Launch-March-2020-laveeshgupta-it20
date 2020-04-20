aa2b19d246da4470c073fc85de16f9f2bfe95f5f
Somnath <somnath@Somnaths-MacBook-Air.local>
2020-04-20 16:38:07 +0530

    Updates

---
+++ precache-manifest.60d5f652a1d612f4bb654ed8e75dba65.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fc4d79cdf6c6142b7d779a23531990",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/index.html"
+  },
+  {
+    "revision": "dc3d871f8d27198f8ba7",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "3eb81edfc1f32e2a521a",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "dc3d871f8d27198f8ba7",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/js/2.99819b59.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/js/2.99819b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb81edfc1f32e2a521a",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/js/main.3b3f8e8e.chunk.js"
+  },
+  {
+    "revision": "2ceaa436b67d09ab6153",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/js/runtime-main.5eb6f5f0.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Launch-March-2020-laveeshgupta-it20/static/media/crio-logo.63be71d0.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Crio-Launch-March-2020-laveeshgupta-it20/precache-manifest.60d5f652a1d612f4bb654ed8e75dba65.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crio-Launch-March-2020-laveeshgupta-it20/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
